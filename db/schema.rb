948fd25261c899a16ffbe4d1a9fb2e84511a2ec6
Antti Leinonen <antti.leinonen@helsinki.fi>
2017-04-07 15:31:43 +0300

    End point for list of organizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126122618) do
+ActiveRecord::Schema.define(version: 20170406120012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -237,6 +237,7 @@ ActiveRecord::Schema.define(version: 20170126122618) do
     t.text     "contact_information"
     t.string   "email"
     t.text     "website"
+    t.boolean  "pinned",              default: false, null: false
   end
 
   create_table "points_upload_queues", force: :cascade do |t|
